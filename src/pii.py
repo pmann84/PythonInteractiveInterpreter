40ff1111333e35780b05e35dff7c1e3c203f0829
pmann84 <peterdmann28@gmail.com>
2016-06-07 07:56:40 +0100

    Moved main script to src and removed path munging Updated .gitignore to ignore .pyc files

--- src/pii.py
+++ src/pii.py
@@ -5,9 +5,7 @@ from PyQt4 import QtGui
 
 # Fudge the python module
 full_script_path = inspect.getfile(inspect.currentframe())
-full_source_path = os.path.abspath(os.path.join(full_script_path, "..\\..\\src"))
 icon_path = os.path.abspath(os.path.join(full_script_path, "..\\..\\icons\\python.png"))
-sys.path.insert(0, full_source_path)
 
 from pii import ui
 from pii.ui import dialog
